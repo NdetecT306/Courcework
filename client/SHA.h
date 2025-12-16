d4c0fc2251502fa5d6dd741c7f48b86ef0d04bd5
NdetecT306 <nstasya777@mail.ru>
2025-12-16 19:21:27 +0700

    Create SHA.h

---
+++ client/SHA.h
@@ -0,0 +1,15 @@
+#ifndef SHA_H
+#define SHA_H
+
+#include <string>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <sstream>
+#include <iomanip>
+#include <bitset>
+#include <algorithm>
+using namespace std;
+string SHA(const string& str);
+
+#endif 
