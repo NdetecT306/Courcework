1a16a6e42221af706c2ebda4c821d8fd4af62fdf
NdetecT306 <nstasya777@mail.ru>
2025-12-19 10:26:44 +0700

    Update NetworkClient.h

--- client/NetworkClient.h
+++ client/NetworkClient.h
@@ -1,24 +1,21 @@
 #ifndef NETWORKCLIENT_H
 #define NETWORKCLIENT_H
+
 #include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 #include <nlohmann/json.hpp>
 #include <string>
 #include <mutex>
-#include <memory>
+#include <thread>
+#include <iostream>
 using namespace std;
 using json = nlohmann::json;
 using boost::asio::ip::tcp;
+namespace ssl = boost::asio::ssl;
 
 class NetworkClient {
-private:
-    boost::asio::io_context io_context;
-    tcp::socket socket;
-    string serverhost;
-    short serverport;
-    bool connected;
-    mutable mutex socketmutex;
 public:
-    NetworkClient(const string& host = "localhost", short port = 8080);
+    NetworkClient(const string& host = "localhost", short port = 8080,const string& cert_file = "server.crt");
     ~NetworkClient();
     bool connect();
     void disconnect();
@@ -30,13 +27,26 @@ public:
     json save_profile(const json& profile);
     json check_connection();
     json get_categories();
-    json get_questions(int category_id);
-    json check_answers(const string& user_login, int category_id, const json& answers);
-    json get_statistics(const string& user_login);
+    json get_questions(int categoryId);
+    json check_answers(const string& userLogin, int categoryId, const json& answers);
+    json get_statistics(const string& userLogin);
     json get_resource_categories();
-    json get_resources_by_category(int category_id);
-    json get_all_resources(); 
+    json get_resources_by_category(int categoryId);
+    json get_all_resources();
     json get_resources_with_categories();
+    
+private:
+    boost::asio::io_context io_context;
+    ssl::context ssl_context;
+    ssl::stream<tcp::socket> socket;
+    thread io_thread;
+    string serverhost;
+    short serverport;
+    string cert_file;
+    bool connected;
+    mutable mutex socketmutex;
 };
+
 extern NetworkClient* g_network_client;
+
 #endif
